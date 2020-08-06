5da2feb903b0e9f575a2f528bdd4e09c66638284
marcogonzaleze <marco.gonzalez.grupoalvamex@gmail.com>
2020-08-06 13:52:22 -0500

    Lote y Vehiculo en Asiento Contable

--- models/account_move.py
+++ models/account_move.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    batch_id = fields.Many2one(comodel_name='stock.picking.batch', related='stock_move_id.batch_id', string="Lote")
-    fleet_id = fields.Many2one(comodel_name='fleet.vehicle', related='stock_move_id.fleet_id', string="Vehiculo")
+    batch_id = fields.Many2one(comodel_name='stock.picking.batch', related='stock_move_id.batch_id', string="Lote", store=True)
+    fleet_id = fields.Many2one(comodel_name='fleet.vehicle', related='stock_move_id.fleet_id', string="Vehiculo", store=True)
